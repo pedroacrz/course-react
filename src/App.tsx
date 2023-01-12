1a535a3de58a2a3f1cd798c1593c21f4c95a100c
pedro.cruz <pedro.cruz@imusica.com.br>
2023-01-12 11:47:25 -0300

    chore: create header component and add react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { FunctionComponent } from 'react'
+import Header from './components/header/header.components'
 
 interface AppProps {
   message: string
 }
 
 const App: FunctionComponent<AppProps> = ({ message }) => {
-  return <h1>hello world</h1>
+  return <Header />
 }
 
 export default App
